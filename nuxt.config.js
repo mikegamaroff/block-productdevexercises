5f02a23ede4fe23df16e9772ef5fc18e233ab722
Mike Gamaroff <mike@gamaroff.net>
2022-06-10 10:41:55 -0700

    Dist add

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,8 +14,9 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   generate: {
-    dir: "public",
+    dir: "dist",
   },
+  target: "static",
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
