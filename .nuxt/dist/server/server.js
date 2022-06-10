184d40e966b17213ca734139315723467a4ae9fa
Mike Gamaroff <mike@gamaroff.net>
2022-06-10 10:39:30 -0700

    publicfolder

--- .nuxt/dist/server/server.js
+++ .nuxt/dist/server/server.js
@@ -839,7 +839,7 @@ async function setContext(app, context) {
   // If context not defined, create it
   if (!app.context) {
     app.context = {
-      isStatic: false,
+      isStatic: true,
       isDev: false,
       isHMR: false,
       app,
@@ -2051,6 +2051,10 @@ const layouts = {
 
     isFetching() {
       return this.nbFetching > 0;
+    },
+
+    isPreview() {
+      return Boolean(this.$options.previewData);
     }
 
   },
